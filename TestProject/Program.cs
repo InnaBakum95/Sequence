28a5469d61c540d026c9501257477a0b7a13aa81
Inna Bakum <ibakum95@gmail.com>
2018-07-16 17:55:52 +0300

    Init

---
+++ TestProject/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = int.Parse(Console.ReadLine());
+            int max = int.Parse(Console.ReadLine());
+            GetPart(min, max);
+
+        }
+
+        private static void GetPart(int min, int max)
+        {
+            List<double> numbers = new List<double>() {1,1,1};
+
+            for (int i = 0; i <= max; i++)
+            {
+                int j = i;
+                numbers.Add(numbers[j] + numbers[j + 1] + numbers[j + 2]);
+            }
+            Console.Write(ReturnNumbers(min, max, numbers));
+            Console.ReadKey();
+        }
+        private static string ReturnNumbers(int min, int max, List<double> numb)
+        {
+            List<double> numbersRet = new List<double>();
+            for (int i = min - 1; i < max; i++)
+            {
+                numbersRet.Add(numb[i]);
+            }
+            return "["+ string.Join("; ", numbersRet) + "]";
+        }
+    }
+}
